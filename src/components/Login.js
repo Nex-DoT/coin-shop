fe983ed693416a2e42024c1f63e7ade5209f52e9
NEX-DoT <nex.dot2@gmail.com>
2023-07-08 19:40:18 +0330

    adding style => Login ,SignUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 const Login = () => {
+    const [data , setData] = useState({
+        email: "" ,
+        password: "" ,
+    })
+const cheangeHandeler = event=>{
+    setData({...data , [event.target.name] : event.target.value})
+}
     return (
-        <div>
-            Login
-        </div>
+        <>
+                    <form>
+        <h2>Sign-Up</h2>
+            <div>
+
+                <div className={style.email}>
+                    <h6>Email</h6>
+                    <input type="email" name="email" value={data.email} onChange={cheangeHandeler}/>
+                </div>
+                <div className={style.pass}>
+                    <h6>Password</h6>
+                    <input type="password" name="password" value={data.password} onChange={cheangeHandeler}/>
+                </div>
+
+            </div>
+
+            <div className={style.btn}> 
+               <button>SignUp</button>
+               <button>Login</button>
+            </div>
+        </form>
+        </>
     );
 };
 
--- src/components/SingUp.js
+++ src/components/SingUp.js
@@ -1,10 +1,48 @@
 import React,{useState , useEffect} from 'react';
-
+//style
+import style from "../styles/Singup.module.css"
 const SingUp = () => {
+    const [data , setData] = useState({
+        username: "" ,
+        email: "" ,
+        password: "" ,
+        cPassword: "" ,
+        checkbox:false
+    })
+const cheangeHandeler = event=>{
+    setData({...data , [event.target.name] : event.target.value})
+}
     return (
         <>
-        <h2>Sing Up now!</h2>
-            
+        <form>
+        <h2>Sign-Up</h2>
+            <div>
+                <div className={style.user}>
+                    <h6>Username</h6>
+                    <input type="text" name="username" value={data.username} onChange={cheangeHandeler}/>
+                </div>
+                <div className={style.email}>
+                    <h6>Email</h6>
+                    <input type="email" name="email" value={data.email} onChange={cheangeHandeler}/>
+                </div>
+                <div className={style.pass}>
+                    <h6>Password</h6>
+                    <input type="password" name="password" value={data.password} onChange={cheangeHandeler}/>
+                </div>
+                <div className={style.pass}>
+                    <h6>Confirm Password</h6>
+                    <input type="password" name="cPassword"  value={data.cPassword} onChange={cheangeHandeler}/>
+                </div>
+            </div>
+            <div className={style.check}>
+                <a href="#">I agree to the terms and conditions.</a>
+                <input type="checkbox" />
+            </div>
+            <div className={style.btn}> 
+               <button>Login</button>
+               <button>SignUp</button>
+            </div>
+        </form>
         </>
     );
 };
