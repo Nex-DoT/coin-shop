047bff30331a94ae447971c211e18183015494a5
NEX-DoT <nex.dot2@gmail.com>
2023-07-11 13:12:51 +0330

    menu style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,27 +1,35 @@
 import React from 'react';
+//Link
 import { Link } from 'react-router-dom';
+//style
 import style from "../styles/Menu.module.css"
+//icon
 import { BsHouseDoorFill } from "react-icons/bs";
 import { BsCurrencyBitcoin } from "react-icons/bs";
 import { BsCartFill } from "react-icons/bs";
+
+//Menu  component
 const Menu = () => {
     return (
+        <div>
+        <span className={style.span1}></span>
         <div className={style.menu}>
             <ul className={style.ul}>
                 <li>
-                    <Link to={"/home"}> <BsHouseDoorFill/></Link>
+                    <Link className={style.icon} to={"/home"}> <BsHouseDoorFill/></Link>
                 </li>
                 <li>
-                    <Link to={"/shop"}> <BsCurrencyBitcoin/></Link>
+                    <Link className={style.icon} to={"/shop"}> <BsCurrencyBitcoin/></Link>
                 </li>
                 <li>
-                    <Link to={"/cart"}> <BsCartFill/> </Link>
+                    <Link className={style.icon} to={"/cart"}> <BsCartFill/> </Link>
                 </li>
                 <li>
-                    <Link to={"/signUp"}>SignUP</Link>/<Link to={"/login"}>Login</Link>
+                    <Link className={style.text} to={"/signUp"}>SignUP</Link><p>/</p><Link className={style.text}to={"/login"}>Login</Link>
                 </li>
             </ul>
             
+        </div>
         </div>
     );
 };
