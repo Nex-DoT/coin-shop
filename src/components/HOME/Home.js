a35133b248e2aaac50d272ccc5e397dbbdf719a8
NEX-DoT <nex.dot2@gmail.com>
2023-07-31 10:01:33 +0330

    added

--- src/components/HOME/Home.js
+++ src/components/HOME/Home.js
@@ -30,6 +30,8 @@ const Home = () => {
                             <li>Axios</li>
                             <li>JavaScript</li>
                             <li>CSS</li>
+                            <li>Grid</li>
+                            <li>Flex box</li>
                             <li>Responsive design</li>
                             <li>HTML</li>
                         </ul> 
@@ -69,38 +71,7 @@ const Home = () => {
                             <li>CSS</li>
                         </ul>
                         <br/>
-                    Now let's go through the components one by one, but before that, let me provide some information about the background.
-                    The background could have been simply included in a component, but for more challenges and practice, we decided to useContext hook, and useEffect hook to handle state changes and display the background using the Ternary operator.
-                </p>
-                <br/>
-                <br/> 
-                <div>
-                    <div className={style.iconDiv}>
-                        <BsCurrencyBitcoin className={style.icon}/>
-                        <h1>Shop component</h1>
-                    </div>
-                    <p>The Shop component receives its data from an API. In our design, when filtering database names in the input field, we receive new information and create search results.</p>
-                </div>
-                    <br/>
-                    <br/>
-                <div>
-                    <div className={style.iconDiv}>
-                        <BsCartFill className={style.icon}/>
-                        <h1>Card component</h1>
-                    </div>
-                            <p>Moving on to the Card component: it consists of three sections. The first section displays the price and quantity of items in the shopping cart; it also includes Pay and Clear buttons that work if there are items in the cart.
-                            The second section displays all available information related to items in the shopping cart; here you can add or remove items. If space is limited on smaller screens, part of this section will be removed so that it doesn't overflow.
-                            Finally, we have a third section displaying three coins that had a significant increase within 24 hours; these coins are suggested as suitable investment options for users.</p>
-                </div>
-                <br/>
-                <br/>
-                <div>
-                    <div className={style.iconDiv}>
-                        <h1>Login and Sign Up page</h1>
-                    </div>
-                    <p>Regarding Sign-up and Login components: validation is implemented using functions specifically designed for them. If all conditions pass successfully (i.e., form inputs are valid), no error will be returned.</p>
-                </div>
-
+                        </p>
             </div>
         </div>
     );
